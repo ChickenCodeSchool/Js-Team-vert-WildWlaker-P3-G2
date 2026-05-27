8c368f153d565b160d59b48c083b888403f89f7f
giobestava <gio.bestava17@gmail.com>
2026-05-27 17:28:17 +0100

    first commit

--- client/src/components/customer/CustomerNavBar.tsx
+++ client/src/components/customer/CustomerNavBar.tsx
@@ -1,8 +1,44 @@
+import "./CustomerNavbar.css";
+import { FiCalendar, FiHome, FiPlus, FiSearch, FiUser } from "react-icons/fi";
+import { NavLink } from "react-router";
+
 function CustomerNavBar() {
   return (
-    <>
-      <h1>Hello from CustomerNavBar</h1>
-    </>
+    <nav className="navbar">
+      <div className="navbar__container">
+        <NavLink to="/" className="navbar__item">
+          <FiHome className="navbar__icon" />
+          <span>Accueil</span>
+        </NavLink>
+
+        <NavLink to="search" className="navbar__item">
+          <FiSearch className="navbar__icon" />
+          <span>Recherche</span>
+        </NavLink>
+
+        <div className="navbar__center">
+          <button
+            type="button"
+            className="navbar__center-button"
+            aria-label="Réserver"
+          >
+            <FiPlus />
+          </button>
+
+          <span className="navbar__center-label">Réserver</span>
+        </div>
+
+        <NavLink to="bookings" className="navbar__item">
+          <FiCalendar className="navbar__icon" />
+          <span>Reservations</span>
+        </NavLink>
+
+        <NavLink to="profile" className="navbar__item">
+          <FiUser className="navbar__icon" />
+          <span>Profil</span>
+        </NavLink>
+      </div>
+    </nav>
   );
 }
 
