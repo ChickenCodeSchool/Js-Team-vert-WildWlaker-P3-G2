b1e999b3a3e4121a5747094202467fb25ed1e536
giobestava <gio.bestava17@gmail.com>
2026-05-29 10:52:18 +0100

    blablabla

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import CustomerNavBar from "./components/customer/CustomerNavBar";
 
 function App() {
   return (
     <main>
       <Outlet />
-      <CustomerNavBar />
     </main>
   );
 }
