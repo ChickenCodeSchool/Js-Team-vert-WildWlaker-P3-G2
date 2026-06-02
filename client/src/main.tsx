33c59d4475b637d084a89c95d7e77f0df1e70aee
giobestava <gio.bestava17@gmail.com>
2026-06-02 15:00:17 +0100

    main changed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
-/* ************************************************************************* */
+/* ********************************************************************** */
 
 // Import the main app component
 import App from "./App";
