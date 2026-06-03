58ab6136a75fd9569cd96f726263ccd9f9a9493f
Hien Tran <hientran@hotmail.fr>
2026-06-03 14:00:02 +0200

    add params in fetch dashboards

--- server/src/modules/appointement/appointementRepository.ts
+++ server/src/modules/appointement/appointementRepository.ts
@@ -2,7 +2,7 @@ import type { Rows } from "../../../database/client";
 import databaseClient from "../../../database/client";
 
 type Appointement = {
-  id_appointement: number;
+  id_appointment: number;
   appointment_date: Date;
   status: string;
   location_type: string;
@@ -28,7 +28,7 @@ class AppointementRepository {
         c.lastname AS customer_lastname,
         u.avatar_url AS customer_avatar, 
         p.name AS prestation_name
-      FROM appointement a
+      FROM appointment a
       JOIN barber b ON a.id_user_barber = b.id_user
       JOIN customer c ON a.id_user_customer = c.id_user
       JOIN users u ON c.id_user = u.id_user  
