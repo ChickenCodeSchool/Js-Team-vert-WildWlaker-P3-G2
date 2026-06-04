433c24beff411188e57d442d354b84109decc4db
Hien Tran <hientran@hotmail.fr>
2026-06-04 15:12:46 +0200

    create and add statscard in admin-user page

--- client/src/pages/admin/users/Users.tsx
+++ client/src/pages/admin/users/Users.tsx
@@ -1,13 +1,46 @@
+import { FiCalendar, FiPause, FiUserPlus } from "react-icons/fi";
 import "./Users.css";
+import StatsCard from "../../../components/admin/statsCard/StatsCard";
 
 function Users() {
   return (
     <div className="admin-users-main">
-      <h2>Gestion des utilisateurs</h2>
-      <p>
-        Cette section est en cours de développement. Restez à l'écoute pour les
-        mises à jour !
-      </p>
+      <div className="admin-users-header">
+        <div className="admin-users-title">
+          <FiUserPlus className="admin-users-title-icon" />
+          <h1>Gestion des utilisateurs</h1>
+        </div>
+        <p>Gérez et suivez les utilisateurs de votre plateforme</p>
+      </div>
+      <div className="admin-users-body">
+        <StatsCard
+          Icon={FiUserPlus}
+          title="utilisateurs"
+          value={150}
+          cycle="Total"
+        />
+        <StatsCard
+          Icon={FiUserPlus}
+          iconColor="icon-success"
+          title="Nouveaux utilisateurs"
+          value={120}
+          cycle="Ce mois-ci"
+        />
+        <StatsCard
+          Icon={FiPause}
+          iconColor="icon-warning"
+          title="Comptes suspendus"
+          value={25}
+          cycle="Total"
+        />
+        <StatsCard
+          Icon={FiCalendar}
+          iconColor="icon-info"
+          title="Réservations"
+          value={30}
+          cycle="Ce mois-ci"
+        />
+      </div>
     </div>
   );
 }
