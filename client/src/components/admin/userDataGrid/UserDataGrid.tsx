03e777782596420a8a6c50ebb896541781591706
Hien Tran <hientran@hotmail.fr>
2026-06-08 16:18:00 +0200

    change icon color in admin user page

--- client/src/components/admin/userDataGrid/UserDataGrid.tsx
+++ client/src/components/admin/userDataGrid/UserDataGrid.tsx
@@ -13,7 +13,7 @@ interface DataGridProps<T> {
   data: T[];
   emptyMessage?: string;
   rowsPerPage: number;
-  onRowClick?: (item: T) => void; // AJOUT : Prop optionnelle pour gérer le clic sur la ligne
+  onRowClick?: (item: T) => void;
 }
 
 function UserDataGrid<T extends { id: string | number }>({
@@ -21,7 +21,7 @@ function UserDataGrid<T extends { id: string | number }>({
   data,
   emptyMessage = "Aucune donnée disponible.",
   rowsPerPage,
-  onRowClick, // AJOUT : Récupération de la prop
+  onRowClick,
 }: DataGridProps<T>) {
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -72,7 +72,6 @@ function UserDataGrid<T extends { id: string | number }>({
             {paginatedData.map((item) => (
               <tr
                 key={item.id}
-                // AJOUT : Gestion du clic et ajout d'une classe CSS conditionnelle
                 onClick={() => onRowClick?.(item)}
                 className={onRowClick ? "clickable-row" : ""}
               >
@@ -111,7 +110,6 @@ function UserDataGrid<T extends { id: string | number }>({
             {visiblePages.map((page) => {
               if (page === "...") {
                 return (
-                  // Utilisation d'un index pour éviter les clés dupliquées si deux "..." apparaissent
                   <span key={`ellipsis`} className="pagination-ellipsis">
                     ...
                   </span>
