5d9415f037747b498e5e8b7fd92ac66f488f98cc
Thomas <thomas.soret@orange.fr>
2026-06-11 16:54:07 +0200

    feat: add customer give avis page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 // Import necessary modules from React and React Router
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router";
 
 /* ********************************************************************** */
 
@@ -11,15 +11,16 @@ import AdminLayout from "./pages/admin/adminLayout/AdminLayout";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import Users from "./pages/admin/users/Users";
 import Login from "./pages/auth/Login";
+import BarberPlanning from "./pages/barber/Planning/BarberPlanning";
 import BarberDashBoard from "./pages/barber/barberDashboard/BarberDashBoard";
 import BarberLayout from "./pages/barber/barberLayout/BarberLayout";
 import BarberPrestation from "./pages/barber/barberPrestations/barbePrestation";
 import BarberProfil from "./pages/barber/barberProfil/BarberProfil";
 import BarberSignalement from "./pages/barber/barberSignalement/BarberSignalement";
-import BarberPlanning from "./pages/barber/Planning/BarberPlanning";
 import CustomerAvis from "./pages/customer/avis/CustomerAvis";
 import BookingPage from "./pages/customer/booking/BookingPage";
 import CustomerLayout from "./pages/customer/customerLayout/CustomerLayout";
+import GiveAvis from "./pages/customer/giveAvis/GiveAvis";
 import Home from "./pages/customer/home/Home";
 import Reservations from "./pages/customer/reservations/Reservations";
 import SearchPage from "./pages/customer/search/SearchPage";
@@ -47,6 +48,7 @@ const router = createBrowserRouter([
           { path: "booking", element: <BookingPage /> },
           { path: "reservations/:id", element: <Reservations /> },
           { path: "avis", element: <CustomerAvis /> },
+          { path: "give-avis", element: <GiveAvis /> },
           { path: "login", element: <Login /> },
         ],
       },
