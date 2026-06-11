b3042d9b2720907c5195f86cd4777838570c4d50
Thomas <thomas.soret@orange.fr>
2026-06-11 17:05:00 +0200

    fix: fix import order for biome 2.4.9 ci

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 // Import necessary modules from React and React Router
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Navigate, RouterProvider, createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 
 /* ********************************************************************** */
 
@@ -11,12 +11,12 @@ import AdminLayout from "./pages/admin/adminLayout/AdminLayout";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import Users from "./pages/admin/users/Users";
 import Login from "./pages/auth/Login";
-import BarberPlanning from "./pages/barber/Planning/BarberPlanning";
 import BarberDashBoard from "./pages/barber/barberDashboard/BarberDashBoard";
 import BarberLayout from "./pages/barber/barberLayout/BarberLayout";
 import BarberPrestation from "./pages/barber/barberPrestations/barbePrestation";
 import BarberProfil from "./pages/barber/barberProfil/BarberProfil";
 import BarberSignalement from "./pages/barber/barberSignalement/BarberSignalement";
+import BarberPlanning from "./pages/barber/Planning/BarberPlanning";
 import CustomerAvis from "./pages/customer/avis/CustomerAvis";
 import BookingPage from "./pages/customer/booking/BookingPage";
 import CustomerLayout from "./pages/customer/customerLayout/CustomerLayout";
