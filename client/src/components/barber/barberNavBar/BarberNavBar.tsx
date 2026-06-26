57712682528eaa01a9c432b3f187bc44635eb3d8
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-06-26 16:59:26 +0200

    responsive for home and barber

--- client/src/components/barber/barberNavBar/BarberNavBar.tsx
+++ client/src/components/barber/barberNavBar/BarberNavBar.tsx
@@ -1,20 +1,27 @@
-import { FiAlertTriangle, FiCalendar, FiHome, FiUser } from "react-icons/fi";
-import "./BarberNavBar.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaCommentAlt, FaCut } from "react-icons/fa";
+import { FiAlertTriangle, FiCalendar, FiHome, FiUser } from "react-icons/fi";
 import { GrDocumentConfig } from "react-icons/gr";
 import { MdFreeCancellation, MdPeopleAlt } from "react-icons/md";
 import { SiSimpleanalytics } from "react-icons/si";
 import { NavLink } from "react-router";
+import "./BarberNavBar.css";
 
 function BarberNavBar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth >= 1024) {
+      setIsOpen(true);
+    }
+  }, []);
+
   return (
     <>
       <button
         type="button"
         className="button_Nav"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
         <FaCut />
       </button>
@@ -25,33 +32,45 @@ function BarberNavBar() {
         <NavLink to="/barber/dashBoard" className="NavBarHair-menu">
           <FiHome /> Tableau de Bord
         </NavLink>
+
         <h3>GESTION</h3>
+
         <NavLink to="/barber/profile" className="NavBarHair-menu">
           <FiUser /> Mon profil
         </NavLink>
+
         <NavLink to="/barber/prestations" className="NavBarHair-menu">
           <MdPeopleAlt /> Mes services
         </NavLink>
+
         <NavLink to="/barber/planning" className="NavBarHair-menu">
           <FiCalendar /> Planning
         </NavLink>
+
         <NavLink to="/barber/disponibilites" className="NavBarHair-menu">
           <FiCalendar /> Disponibilités
         </NavLink>
+
         <NavLink to="/barber/annulations" className="NavBarHair-menu">
           <MdFreeCancellation /> Gestion RDV
         </NavLink>
+
         <h3>ANALYTICS</h3>
+
         <NavLink to="/barber/signalement" className="NavBarHair-menu">
           <FiAlertTriangle /> Signalements
         </NavLink>
+
         <NavLink to="/barber/statistics" className="NavBarHair-menu">
           <SiSimpleanalytics /> Statistique
         </NavLink>
+
         <NavLink to="/barber/customer" className="NavBarHair-menu">
           <FaCommentAlt /> Avis
         </NavLink>
+
         <h3>CONFIGURATION</h3>
+
         <NavLink to="/barber/parametres" className="NavBarHair-menu">
           <GrDocumentConfig />
           Paramètre
