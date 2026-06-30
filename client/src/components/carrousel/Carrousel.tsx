4e71d5d688308740014d8748dcc085190cb9a713
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-06-30 11:15:05 +0200

    search css

--- client/src/components/carrousel/Carrousel.tsx
+++ client/src/components/carrousel/Carrousel.tsx
@@ -20,10 +20,11 @@ function Carrousel({
           <button
             type="button"
             key={barber.id_user}
-            className={`carrousel__card ${selectedBarber?.id_user === barber.id_user
+            className={`carrousel__card ${
+              selectedBarber?.id_user === barber.id_user
                 ? "carrousel__card--selected"
                 : ""
-              }`}
+            }`}
             onClick={() => onSelectBarber?.(barber)}
           >
             <div className="carrousel__image-wrapper">
