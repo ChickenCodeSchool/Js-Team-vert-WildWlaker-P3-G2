64ec16736d633d1740e1d0397c4700f2b41ecd7b
Hien Tran <hientran@hotmail.fr>
2026-07-06 12:30:50 +0200

    refacto routerAdmin

--- server/src/routerAdmin.ts
+++ server/src/routerAdmin.ts
@@ -13,91 +13,32 @@ import { uploadEventImage } from "./modules/event/eventUpload";
 import reviewActions from "./modules/review/reviewActions";
 import userActions from "./modules/user/userActions";
 
-routerAdmin.get(
-  "/api/admin/dashboard",
-  verifyToken,
-  verifyAdmin,
-  adminDashboardAction.browse,
-);
+routerAdmin.use(verifyToken, verifyAdmin);
+
+routerAdmin.get("/api/admin/dashboard", adminDashboardAction.browse);
 routerAdmin.get(
   "/api/admin/appointmentsdetails",
-  verifyToken,
-  verifyAdmin,
   appointmentActions.browseforadmin,
 );
-routerAdmin.get(
-  "/api/admin/appointments",
-  verifyToken,
-  verifyAdmin,
-  appointmentActions.browse,
-);
-routerAdmin.get(
-  "/api/admin/barbers",
-  verifyToken,
-  verifyAdmin,
-  barberActions.browse,
-);
-routerAdmin.put(
-  "/api/admin/barbers/:id",
-  verifyToken,
-  verifyAdmin,
-  barberActions.edit,
-);
-routerAdmin.delete(
-  "/api/admin/users/:id",
-  verifyToken,
-  verifyAdmin,
-  userActions.deleteUser,
-);
+routerAdmin.get("/api/admin/appointments", appointmentActions.browse);
+routerAdmin.get("/api/admin/barbers", barberActions.browse);
+routerAdmin.put("/api/admin/barbers/:id", barberActions.edit);
+routerAdmin.delete("/api/admin/users/:id", userActions.deleteUser);
 routerAdmin.post(
   "/api/admin/events",
-  verifyToken,
-  verifyAdmin,
   uploadEventImage.single("image"),
   eventActions.add,
 );
 routerAdmin.put(
   "/api/admin/events/:id",
-  verifyToken,
-  verifyAdmin,
   uploadEventImage.single("image"),
   eventActions.edit,
 );
-routerAdmin.delete(
-  "/api/admin/events/:id",
-  verifyToken,
-  verifyAdmin,
-  eventActions.destroy,
-);
-routerAdmin.get(
-  "/api/admin/customers",
-  verifyToken,
-  verifyAdmin,
-  customerActions.browse,
-);
-routerAdmin.put(
-  "/api/admin/customers/:id",
-  verifyToken,
-  verifyAdmin,
-  customerActions.edit,
-);
-routerAdmin.get(
-  "/api/admin/reviews",
-  verifyToken,
-  verifyAdmin,
-  reviewActions.browse,
-);
-routerAdmin.delete(
-  "/api/admin/reviews/:id",
-  verifyToken,
-  verifyAdmin,
-  reviewActions.destroy,
-);
-routerAdmin.get(
-  "/api/admin/reviewsdetails",
-  verifyToken,
-  verifyAdmin,
-  reviewActions.browseforadmin,
-);
+routerAdmin.delete("/api/admin/events/:id", eventActions.destroy);
+routerAdmin.get("/api/admin/customers", customerActions.browse);
+routerAdmin.put("/api/admin/customers/:id", customerActions.edit);
+routerAdmin.get("/api/admin/reviews", reviewActions.browse);
+routerAdmin.delete("/api/admin/reviews/:id", reviewActions.destroy);
+routerAdmin.get("/api/admin/reviewsdetails", reviewActions.browseforadmin);
 
 export default routerAdmin;
